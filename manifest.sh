15fb115b5892dfd34effdfdbedd7a135ca854356
Casey Waldren <caseywaldren@gmail.com>
2025-01-24 19:53:11 -0800

    fix manifest generator again

--- manifest.sh
+++ manifest.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-if [[ -z "$1" ]] 
-then	
-	echo "Supply the URL of the Skyward instance."
-	exit 1
+if [[ -z "$1" ]]; then
+  echo "Supply the URL of the Skyward instance."
+  exit 1
 fi
 
-sed "s/YOUR_URL_GOES_HERE/$1/g" manifest.json.template > manifest.json
+sed -e 's|YOUR_URL_GOES_HERE|'"$1"'|g' manifest.json.template >manifest.json
 
 echo "manifest.json generated."
-
-
